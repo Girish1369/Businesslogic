fe01a00e50cf777867406d82a9cab8716e3bbc31
Girish1369 <girishkatkamwar.1369@gmail.com>
2023-05-26 11:02:59 +0530

    muliplication class is added

---
+++ src/com/demo/multiplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class multiplication {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int multi = a*b;
+		System.out.println("mulitplication="+multi);
+
+	}
+
+}
